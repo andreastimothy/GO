99f30430301233dde77ebfb45d22edb2a5057d8c
andreastimothy <109422303+andreastimothy@users.noreply.github.com>
2022-09-01 00:14:36 +0700

    Delete go-mux-postgresql directory

--- go-mux-postgresql/internal/model/product.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name" validate:"required"`
-	Description string `json:"description" validate:"required"`
-	Quantity    int    `json:"quantity" validate:"required"`
-}
--- go-mux-postgresql/internal/route/init.go
+++
@@ -1,9 +0,0 @@
-package route
-
-import (
-	"github.com/gorilla/mux"
-)
-
-func InitRoute(r *mux.Router) {
-	InitProductRoute(r)
-}
